2363bedadcde31a71c5bfe9ac1a0856c87302395
gasparbarancelli@gmail.com <g45p4r19121987>
2019-11-07 08:36:22 -0300

    init

---
+++ src/main/java/br/com/viasoft/bootcamp/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package br.com.viasoft.bootcamp.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulServer
+@EnableZuulProxy
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+}
